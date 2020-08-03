e92f7e0245df0cb7c8000c96131ba46d5501335f
DieGo <die_go_p@hotmail.com>
2020-08-02 19:12:25 -0500

    Doc added

---
+++ docs/precache-manifest.97434350282ad373683c480bad66b20f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bc8b6cb14fddd3c72b3cd72f905a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e35589840abfb18024",
+    "url": "/static/css/main.0d0fbf20.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e35589840abfb18024",
+    "url": "/static/js/main.d263b791.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
